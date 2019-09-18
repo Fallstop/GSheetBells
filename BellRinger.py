40e6db241a3efb648d98ad8c5780cd27fb5f11ee
Fallstop <43323210+Fallstop@users.noreply.github.com>
2019-09-19 11:56:01 +1200

    Update BellRinger.py

--- BellRinger.py
+++ BellRinger.py
@@ -19,14 +19,12 @@ def RingBell(TimeToRingFor):
     print("Ringing")
     time.sleep(TimeToRingFor)
     print("StopRinging")
-def CheckBell(Time,Data):
+def CheckBell(Time,Day,Data):
     i = 0
     while i < Data.shape[0]:
         print("Line:",i,"Time:",Data.iloc[i,0])
         i += 1
-    for x in Data:
-        print(Data.iloc[x,0])
 Data = ReadData()
 Time = GetTime()
 Day = 1
-CheckBell(Time,Data)
+CheckBell(Time,Day,Data)
