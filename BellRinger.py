e0cbfd47abb46a478d9c71b6449675883a3c9915
Fallstop <43323210+Fallstop@users.noreply.github.com>
2019-09-19 14:37:28 +1200

    Rudementry Bell ringing set up

--- BellRinger.py
+++ BellRinger.py
@@ -8,7 +8,7 @@ def GetTime(): ##Function for reciveing
     return(Time)
 def ReadData():
     try:
-        print("Read Sucseded")
+        print("Reading data")
         return(df.read_csv("BellStorage.csv",header=None)) ##File with times and dates for all bells
     except:
         print("Read failed")
@@ -20,11 +20,28 @@ def RingBell(TimeToRingFor):
     time.sleep(TimeToRingFor)
     print("StopRinging")
 def CheckBell(Time,Day,Data):
-    i = 0
-    while i < Data.shape[0]:
-        print("Line:",i,"Time:",Data.iloc[i,0])
+    
+    BeginTime = time.time()
+    i = 1
+    BellDayData = Data[[1]].copy()
+    BellDayData = BellDayData.iloc[:,0]
+    BellDayData = BellDayData.str.split(pat = "/")
+    while i < BellDayData.shape[0]:
+        if Data.iloc[i,0] == Time:
+            x = 0
+            BellDay = BellDayData.iloc[i]
+            while x < len(BellDay) and Day >= int(BellDay[x]):
+                if int(BellDay[x]) == Day:
+                    RingBell(Data.iloc[i,2])
+                    print("CheckBell Function found a match in",round(time.time()-BeginTime,5),"secs at ",Time)
+                    break
+                x += 1
         i += 1
-Data = ReadData()
-Time = GetTime()
-Day = 1
-CheckBell(Time,Day,Data)
+    print("Check Bell function did not find a match in",round((time.time()-BeginTime),5),"secs at ",Time)
+while True:
+    Data = ReadData()
+    Time = GetTime()
+    Day = 3
+    CheckBell(Time,Day,Data)
+    time.sleep(3)
+
