bcdac8f8ec988145959ba3336a0acd06adfd2982
Fallstop <jasper@ngita.co.nz>
2020-02-02 09:52:11 +1300

    Added Startup logs

--- BellRinger.sh
+++ BellRinger.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd Python
-python3 CrashFixerBellRinger.py
+python3 CrashFixerBellRinger.py >> BellRingerStartLog.txt
--- StartScripts.sh
+++ StartScripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-screen -dmS "BellRinger" sh -c "./BellRinger.sh; exec bash"
+screen -dmS "BellRinger" sh -c "./BellRinger.sh; exec bash" >> StartScriptLog.txt
 #screen -dmS "DataManager" sh -c "./DataManager; exec bash"
