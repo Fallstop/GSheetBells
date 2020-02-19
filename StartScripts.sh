9279e36787d6cf3181c739208bcfb58659785aaf
Fallstop <jasper@ngita.co.nz>
2020-02-19 15:34:14 +1300

    yes1

--- StartScripts.sh
+++ StartScripts.sh
@@ -2,5 +2,5 @@
 cd home/pi/Documents/HCBellsPython/
 if ! screen -list | grep -q "BellRinger"; then
     screen -dmS "BellRinger" sh -c "./Documents/HCBellsPython/BellRinger.sh; exec bash" >> StartScriptLog.txt
-    #screen -dmS "DataManager" sh -c "./DataManager; exec bash"
+    screen -dmS "InternetStatus" sh -c "./Documents/HCBellsPython/InternetStatus.sh; exec bash"
 fi
